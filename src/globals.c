d50d889ab5e8f047719254fd4c7d0fb8f4248688
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-05-04 16:07:14 +0200

    globals

--- src/globals.c
+++ src/globals.c
@@ -11,7 +11,23 @@ typedef struct {
   int y;
 } BlockPos;
 
+static Player player = {
+  (Vector2){0, 0},
+  (Vector2){0, 0},
+};
+
 bool IsBlockSolid(BlockPos pos) {
   if (pos.x > 0) return true;
   return false;
+}
+
+BlockPos GetBlockPos(Vector2 pos) {
+  return (BlockPos){
+    .x = (int)pos.x,
+    .y = (int)pos.y,
+  };
+}
+
+bool IsPosSolid(Vector2 pos) {
+  return IsBlockSolid(GetBlockPos(pos));
 }
